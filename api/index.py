7fe1e350e3952781a85dcc411c7da69f5139e21d
Dr-Insanity <cichonquincy@gmail.com>
2023-09-07 14:10:04 +0200

    Try dis

--- api/index.py
+++ api/index.py
@@ -7,12 +7,24 @@ app = Flask(__name__)
 def embed():
     base_url = "https://discordembeds.vercel.app/embed?"
     query = {}
-    embed = "<head>"
+
+    # Process query parameters provided in the request
     for embed_feature, embed_value in request.args.items():
         query[embed_feature] = embed_value
-        embed += "<meta name={} content={}>".format(embed_feature, embed_value)
-    embed += f"""</head>\n<body>\n<p>{query["title"]}</p>\n<p>{query["description"]}</p>\n</body>"""
-    embed_url = f"{base_url+urllib.parse.urlencode(query)}"
+
+    # Build the HTML embed code
+    embed = "<head>"
+    for key, value in query.items():
+        embed += f"<meta name='{key}' content='{value}'>"
+    embed += "</head><body>"
+    if 'title' in query:
+        embed += f"<p>{query['title']}</p>"
+    if 'description' in query:
+        embed += f"<p>{query['description']}</p>"
+    embed += "</body>"
+
+    # Build the final URL, including only provided parameters
+    embed_url = f"{base_url}{urllib.parse.urlencode(query)}"
 
     return embed
 
