4f6463a1bdba8ba0d35d57ec4568ded170fa913a
Dr-Insanity <cichonquincy@gmail.com>
2025-03-26 17:54:07 +0100

    sorta pre-releasing/testing image feature. Should work. Will make the option later on the '/' route to graphically enable this or not.

--- api/index.py
+++ api/index.py
@@ -15,28 +15,31 @@ def embed():
     ]
 
     queries_to_metas = {
-        "title": "title",
-        "description": "description",
-        "color": "theme-color",
+        "title": '''name="title"''',
+        "description": '''name="description"''',
+        "color": '''name="theme-color"''',
+        "image": '''property="twitter:image"''',
     }
 
     query = {
         queries_to_metas["title"]: "",
         queries_to_metas["description"]: "",
         queries_to_metas["color"]: "",
+        queries_to_metas["image"]: "",
     }
 
     for embed_feature, embed_value in request.args.items():
         try:
-            print("query: %s" % embed_feature)
-            print("value: %s" % embed_value)
             query[queries_to_metas[embed_feature]] = embed_value
         except KeyError:
             continue
 
     embed = "<head>\n   "
     for key, value in query.items():
-        embed += f"""<meta name={key} content="{html.escape(value)}">"""
+        if key == '''property="twitter:image"''':
+            embed += f"""<meta property="twitter:image" content="{html.escape(value)}">\n   <meta property="og:image" content="{html.escape(value)}">"""
+        else:
+            embed += f"""<meta {key} content="{html.escape(value)}">"""
     embed += "</head>"
 
     # Build the final URL, including only provided parameters
